2b5eb6fc898e3afc26c414907c3e3054f6f40432
TatianaAll <tatiana.allery@orange.fr>
2024-11-25 14:09:55 +0100

    add entry condition

--- src/Controller/PokerController.php
+++ src/Controller/PokerController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 //j'appelle les classes Symfony que je vais utiliser
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 //je déclare ma class PokerController
 class PokerController
@@ -16,10 +17,16 @@ class PokerController
         //appel de la methode createFromGloblal de la class Request
         // sans avoir besoin d'instancier une nouvelle class Request
         //et ça grace au double points ::
+        //la class Request avec sa method createFromGlobals permet de stocker
+        //les infos en POST, GET,
         $request = Request::createFromGlobals();
         //je récupère ma variable age à partir des infos du get de mon URL
         $age = $request->query->get('age');
         //je les affiche avec un var_dum et je tue mon process
-        var_dump($age); die;
+        if($age < 18){
+            return new Response('<h1>Ma page d\'accueil</h1> <p>Vous n\'avez pas l\'âge requis</p>');
+        } else {
+            return new Response('<h1>Ma page d\'accueil</h1> <p>Bienvennue sur POKER WORD THE MOST FAMOUS POKER WEBSITE !</p>');
+        }
     }
 }
