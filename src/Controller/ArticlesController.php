85836c4f30b930230a614c45bfe1cf7018bd2fdb
TatianaAll <tatiana.allery@orange.fr>
2024-11-26 15:55:16 +0100

    add created date in articles and show in view

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -9,57 +9,98 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ArticlesController extends AbstractController
 {
     //je met mes article dans une propriété
-    public array $articles = [
-        [
-            'id' => 1,
-            'title' => 'Article 1',
-            'content' => 'Content of article 1',
-            'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
-        ],
-        [
-            'id' => 2,
-            'title' => 'Article 2',
-            'content' => 'Content of article 2',
-            'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
-        ],
-        [
-            'id' => 3,
-            'title' => 'Article 3',
-            'content' => 'Content of article 3',
-            'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
-        ],
-        [
-            'id' => 4,
-            'title' => 'Article 4',
-            'content' => 'Content of article 4',
-            'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
-        ],
-        [
-            'id' => 5,
-            'title' => 'Article 5',
-            'content' => 'Content of article 5',
-            'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
-        ]
 
-    ];
 
     #[Route('/articles', name: 'articles')]
     public function getArticles()
     {
-        return $this->render('articles.html.twig', ['articles' => $this->articles]);
+        $articles = [
+            [
+                'id' => 1,
+                'title' => 'Article 1',
+                'content' => 'Content of article 1',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
+            ],
+            [
+                'id' => 2,
+                'title' => 'Article 2',
+                'content' => 'Content of article 2',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
+            ],
+            [
+                'id' => 3,
+                'title' => 'Article 3',
+                'content' => 'Content of article 3',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
+            ],
+            [
+                'id' => 4,
+                'title' => 'Article 4',
+                'content' => 'Content of article 4',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
+            ],
+            [
+                'id' => 5,
+                'title' => 'Article 5',
+                'content' => 'Content of article 5',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
+            ]
+        ];
+        return $this->render('articles.html.twig', ['articles' => $articles]);
     }
 
     #[Route('/article/{id}', name: 'article_show')]
     public function showArticle($id)
     {
         //je recupere mes articles
-        $this->getArticles();
+        $articles = [
+            [
+                'id' => 1,
+                'title' => 'Article 1',
+                'content' => 'Content of article 1',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
+            ],
+            [
+                'id' => 2,
+                'title' => 'Article 2',
+                'content' => 'Content of article 2',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
+            ],
+            [
+                'id' => 3,
+                'title' => 'Article 3',
+                'content' => 'Content of article 3',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
+            ],
+            [
+                'id' => 4,
+                'title' => 'Article 4',
+                'content' => 'Content of article 4',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
+            ],
+            [
+                'id' => 5,
+                'title' => 'Article 5',
+                'content' => 'Content of article 5',
+                'image' => 'https://static.vecteezy.com/system/resources/thumbnails/012/176/986/small_2x/a-3d-rendering-image-of-grassed-hill-nature-scenery-png.png',
+                'createdAt' => new \DateTime('2030-01-01 00:00:00')
+            ]
+        ];
 
         // j'initie un articleFound à null
         $articleFound = null;
 
         // je parcours mes articles récupérés
-        foreach ($this->articles as $article) {
+        foreach ($articles as $article) {
             // si l'id de mon article correspond à l'id stocké depuis le get
             if ($article['id'] === (int)$id) {
                 //alors je donne à mon articleFound les données de mon article avec le meme id
@@ -67,17 +108,19 @@ class ArticlesController extends AbstractController
                 return $this->render('article_show.html.twig', ['article' => $article]);
             }
             //si je n'ai rien qui correspond je renvoie une page d'erreur
-        } return $this->render('error_404.html.twig');
+        }
+        return $this->render('error_404.html.twig');
     }
 
     //on va faire une nouvelle page pour filtrer par categorie : 1- routing
     #[Route('/articles/search_results', name: 'articles_search_result')]
     // j'utilise dans ma methode un autowire
-    //je vais donc lui passer en parametre $request en précisant avec le typage que
-    //$request est une instance de la class Request
-    //symfony va m'autocompléter tout pour me faire cette instance de classe sans que j'ai besoin
-    // de préciser les parametres du constructeur
-    public function searchArticle(Request $request){
+        //je vais donc lui passer en parametre $request en précisant avec le typage que
+        //$request est une instance de la class Request
+        //symfony va m'autocompléter tout pour me faire cette instance de classe sans que j'ai besoin
+        // de préciser les parametres du constructeur
+    public function searchArticle(Request $request)
+    {
 
         //ma variable search contient les informations de get de la requete HTTP
         $search = $request->query->get('search');
