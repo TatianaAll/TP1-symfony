ebd00c436a97167abc70438e1cf2815ceaeb0a4a
TatianaAll <tatiana.allery@orange.fr>
2024-11-26 10:23:43 +0100

    add 'article' and view corresponding for shwo article by id

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -44,7 +45,35 @@ class ArticlesController extends AbstractController
 
     //je fais ma méthode getArticle en lui donnant une route
     #[Route('/articles', name: 'articles')]
-    public function getArticles(){
+    public function getArticles()
+    {
         return $this->render('articles.html.twig', ['articles' => $this->articles]);
+    }
+
+    // je fais une méthode pour filtrer mes articles par ID
+    #[Route('/article', name: 'article_show')]
+    public function showArticle()
+    {
+        $this->getArticles();
+
+        // j'initie un articleFound à null
+        $articleFound = null;
+
+        // je vais récupérer mes commande de server, donc notamment mes entrées en GET
+        // pour ça j'utilise la class Request et la methode createFromGlobals
+        $request = Request::createFromGlobals();
+        //ici je stocke dans une variable id la valeur GET récupérée par createFromGlobals
+        // et je prend spécifiquement la valeur pour la clé 'id'
+        $id = $request->query->get('id');
+
+        // je parcours mes articles récupérés
+        foreach ($this->articles as $article) {
+            // si l'id de mon article correspond à l'id stocké depuis le get
+            if ($article['id'] === (int)$id) {
+                //alors je donne à mon articleFound les données de mon article avec le meme id
+                $articleFound = $article;
+                return $this->render('article_show.html.twig', ['article' => $article]);
+            }
+        }
     }
 }
