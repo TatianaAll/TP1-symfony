2d56e88c5214d996c28f01c1b662fb6d180720c2
TatianaAll <tatiana.allery@orange.fr>
2024-11-27 12:40:44 +0100

    install docktrin and change in CSS

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -11,35 +11,18 @@ use Symfony\Component\HttpFoundation\Request;
 class ContactController extends AbstractController
 {
     //je défini la route de ma nouvelle méthode
-    #[Route('/contact_form', 'contactForm')]
+    #[Route(path : '/contact_form', name: 'contactForm')]
     public function showContactForm(Request $request)
     {
         $name = $request->request->get('name');
         $email = $request->request->get('email');
         $subject = $request->request->get('subject');
         $message_area = $request->request->get('message_area');
+
         //j'affiche mon formulaire de contact
         return $this->render('contact_form.html.twig',['name'=>$name, 'email'=>$email, 'subject'=>$subject,
                 'message_area'=>$message_area]);
     }
 
-
-    // je définie une nouvelle route pour mon résultat de soumission de form
-//    #[Route('/contact_submit', 'contactSubmit')]
-//    // ma méthode sera une instance de Request donc je fais un autowire
-//    public function getContactForm(Request $request)
-//    {
-//        //Je récupère mes données de mon formulaire POST avec request
-//        $name = $request->request->get('name');
-//        $email = $request->request->get('email');
-//        $subject = $request->request->get('subject');
-//        $message_area = $request->request->get('message_area');
-//
-//        //je renvoie ma page avec les variables
-//        return $this->render('contact_submit.html.twig',
-//            ['name'=>$name, 'email'=>$email, 'subject'=>$subject,
-//                'message_area'=>$message_area]);
-//
-//    }
 }
 
