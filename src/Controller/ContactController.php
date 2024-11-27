98d7f35f10d4f6f392e62fbf2dc2c35b3885dbb9
TatianaAll <tatiana.allery@orange.fr>
2024-11-27 11:27:16 +0100

    change logic in ContactController to do all in the same page

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -10,32 +10,36 @@ use Symfony\Component\HttpFoundation\Request;
 //comme ça je pourrais appeler les render depuis cette nouvelle classe
 class ContactController extends AbstractController
 {
-    //je définie la route de ma nouvelle méthode
+    //je défini la route de ma nouvelle méthode
     #[Route('/contact_form', 'contactForm')]
-    public function showContactForm()
+    public function showContactForm(Request $request)
     {
-        return $this->render('contact_form.html.twig');
-    }
-
-    #[Route('/contact_submit', 'contactSubmit')]
-    public function getContactForm(Request $request)
-    {
-
         $name = $request->request->get('name');
         $email = $request->request->get('email');
         $subject = $request->request->get('subject');
         $message_area = $request->request->get('message_area');
-
-
-        return $this->render('contact_submit.html.twig',
-            ['name'=>$name, 'email'=>$email, 'subject'=>$subject,
+        //j'affiche mon formulaire de contact
+        return $this->render('contact_form.html.twig',['name'=>$name, 'email'=>$email, 'subject'=>$subject,
                 'message_area'=>$message_area]);
-//        if ($email && $subject && $message_area) {
-//
-//        } else return $this->redirectToRoute('not_found');
-
-
-
     }
+
+
+    // je définie une nouvelle route pour mon résultat de soumission de form
+//    #[Route('/contact_submit', 'contactSubmit')]
+//    // ma méthode sera une instance de Request donc je fais un autowire
+//    public function getContactForm(Request $request)
+//    {
+//        //Je récupère mes données de mon formulaire POST avec request
+//        $name = $request->request->get('name');
+//        $email = $request->request->get('email');
+//        $subject = $request->request->get('subject');
+//        $message_area = $request->request->get('message_area');
+//
+//        //je renvoie ma page avec les variables
+//        return $this->render('contact_submit.html.twig',
+//            ['name'=>$name, 'email'=>$email, 'subject'=>$subject,
+//                'message_area'=>$message_area]);
+//
+//    }
 }
 
