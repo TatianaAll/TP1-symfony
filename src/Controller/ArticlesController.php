94e72851837242d8a56b7fda8b0960a04a5ecc44
TatianaAll <tatiana.allery@orange.fr>
2024-11-28 17:18:57 +0100

    createArticle update -> work with a form

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,7 +17,7 @@ class ArticlesController extends AbstractController
     #[Route('/articles', name: 'articles')]
     public function getArticles(ArticleRepository $articleRepository): Response
     {
-        $articles=$articleRepository->findAll();
+        $articles = $articleRepository->findAll();
         return $this->render('articles.html.twig', ['articles' => $articles]);
     }
 
@@ -29,7 +30,8 @@ class ArticlesController extends AbstractController
 
         if ($articleFound === null) {
             return $this->redirectToRoute('error_404.html.twig');
-        } return $this->render('article_show.html.twig', ['article' => $articleFound]);
+        }
+        return $this->render('article_show.html.twig', ['article' => $articleFound]);
     }
 
     //on va faire une nouvelle page pour filtrer par categorie : 1- routing
@@ -49,37 +51,43 @@ class ArticlesController extends AbstractController
 
     }
 
-    #[Route(path:'/article/create', name:'article_create')]
-    public function createArticle(EntityManagerInterface $entityManager): Response{
-        //dd("coucou");
-
-        //créer un article
-        //en faisant une nouvelle instance de l'entité Article
+    #[Route(path: '/article/create', name: 'article_create')]
+    public function createArticle(EntityManagerInterface $entityManager, Request $request): Response
+    {
         $article = new Article();
+        //dd("coucou");
+        if ($request->isMethod('POST')) {
+            //créer un article
+            //en faisant une nouvelle instance de l'entité Article
 
-        //Puis on rempli notre nouvel article avec les méthodes set de la classe Article
-        $article->setTitle('Un article fait à la mains');
-        $article->setContent('Un article de qualité, créé à la main, moulé dans un moule et sorti tout chaud du four de Symfony');
-        $article->setCreatedAt(new \DateTime());
-        $article->setImage('https://labonneflambee.fr/49-large_default/four-a-tarte-flambee-et-pizza-le-wackes.jpg');
-        //on vérifie que ça fonctionne bien
-        //dd($article);
 
-        //une fois notre instance créée, l'ORM pourra faire correspondre cette instance avec le SQL
-        //utilisation de EntityManagerInterface
-        //on ''''''commit'''''', on envoie nos modif
-        $entityManager->persist($article);
-        //puis on '''push''' donc on envoie à notre BDD le nouvel article
-        $entityManager->flush();
+            $newTitle = $request->request->get('title');
+            $newContent = $request->request->get('content');
+            $newImage = $request->request->get('image');
+
+            //Puis on rempli notre nouvel article avec les méthodes set de la classe Article
+            $article->setTitle($newTitle);
+            $article->setContent($newContent);
+            $article->setImage($newImage);
+            $article->setCreatedAt(new DateTime('now'));
+
+            //une fois notre instance créée, l'ORM pourra faire correspondre cette instance avec le SQL
+            //utilisation de EntityManagerInterface
+            //on ''''''commit'''''', on envoie nos modif
+            $entityManager->persist($article);
+            //puis on '''push''' donc on envoie à notre BDD le nouvel article
+            $entityManager->flush();
+            return $this->render('article_create.html.twig', ['article' => $article]);
+        }
 
         return $this->render('article_create.html.twig', ['article' => $article]);
     }
 
     //je fais ma route pour ma suppression d'article, je donne un id en URL en m'assurant que c'est un integer avec une regex
-    #[Route (path:'/article/delete/{id}', name: 'article_delete', requirements: ['id'=>'\d+'])]
+    #[Route (path: '/article/delete/{id}', name: 'article_delete', requirements: ['id' => '\d+'])]
     //je doit avoir un id
-    //j'appelle une instance de mon Article Repo car je vais avoir besoin de parcourir toutes mes instances de l'entité Article qui sont créée
-   //j'appelle une instance d'EntityManager pour pouvoir modifier ma BDD
+        //j'appelle une instance de mon Article Repo car je vais avoir besoin de parcourir toutes mes instances de l'entité Article qui sont créée
+        //j'appelle une instance d'EntityManager pour pouvoir modifier ma BDD
     public function deleteArticle(int $id, ArticleRepository $articleRepository, EntityManagerInterface $entityManager): Response
     {
         //dd("coucou");
@@ -100,7 +108,7 @@ class ArticlesController extends AbstractController
         return $this->render('article_delete.html.twig', ['article' => $articleToDelete]);
     }
 
-    #[Route(path:'/article/update/{id}', name: 'article_update', requirements: ['id'=>'\d+'])]
+    #[Route(path: '/article/update/{id}', name: 'article_update', requirements: ['id' => '\d+'])]
     function updateArticle(int $id, EntityManagerInterface $entityManager, ArticleRepository $articleRepository): Response
     {
         //dd("coucou");
